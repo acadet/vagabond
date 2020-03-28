16ac968a9c28259cd525db5cb585751e98b41d13
Adrien Cadet <acadet@live.fr>
2020-03-28 18:20:44 -0400

    Port AZ road trip

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
         { name: 'dc', date: '2016' },
         { name: 'seattle', date: '2016' },
         { name: 'spain-portugal', date: '2016' },
-        // { name: 'arizona', date: '2015' },
+        { name: 'arizona', date: '2015' },
         // { name: 'chicago', date: '2015' },
         // { name: 'nyc', date: '2015' },
         // { name: 'sf', date: '2015' }
